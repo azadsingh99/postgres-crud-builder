7382aa80ca7a4c6b78204eb2d540ea4690330492
azadsingh99 <azadsingh1934278@gmail.com>
2024-07-11 21:27:25 +0530

    Resolved issue & made connection properly between routes & controller

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const crudBuilderRoute = require('./routes/crudBuilderRoute');
 
 
+app.use(crudBuilderRoute);
 app.listen(3000, () => {
     console.log('listening on port 3000');
 })
